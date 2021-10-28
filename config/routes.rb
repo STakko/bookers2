927f973baa1b20294d31474b89327947e1b7f200
STakko <91719838+STakko@users.noreply.github.com>
2021-10-28 18:52:01 +0900

    [Add]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'homes#top'
+  devise_for :users
+  resources :books, only: [:new, :index, :show, :edit, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
