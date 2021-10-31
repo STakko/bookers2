7ec2c7f1c59d9a2ff6421dae080f4722d1bba274
STakko <91719838+STakko@users.noreply.github.com>
2021-10-31 12:17:14 +0900

    [Fix]4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+  end
+
+  def user_books
+    @user = User.find(params[id])
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  root 'homes#top'
-  get 'homes/about' => 'homes#about'
   devise_for :users
-  resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
+  root to: 'homes#top'
+  get 'homes/about' => 'homes#about'
+  get 'users/:id/edit' => 'users#edit', as:'edit_user_path'
+  get 'users/:id' => 'users#show', as:'user_path'
+  get 'users' => 'users#index', as:'users_path'
+  get 'users/:id' => 'users#user_books', as'user_books_path'
+
+  get 'books/:id/edit' => 'books#edit', as:'books_edit_path'
+  get 'books/:id' => 'books#show', as:'book_path'
+  get 'books' => 'books#index', as:'books_path'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
